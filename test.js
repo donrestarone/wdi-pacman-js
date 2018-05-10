51a0bf2b4d65234da64d8c91fcffe3922fad49aa
donrestarone <shashikejayatunge@gmail.com>
2018-05-09 22:05:39 -0400

    start assignment, set up ghosts

---
+++ test.js
@@ -0,0 +1,36 @@
+var inky = {
+  menu_option: '1',
+  name: 'Inky',
+  colour: 'Red',
+  character: 'Shadow',
+  edible: 'false',
+};
+
+var blinky = {
+  menu_option: '2',
+  name: 'Blinky',
+  colour: 'Cyan',
+  character: 'Speedy',
+  edible: 'false',
+}
+
+var pinky = {
+  menu_option: '3',
+  name: 'Pinky',
+  colour: 'Pink',
+  character: 'Bashful',
+  edible: 'false',
+}
+
+var clyde = {
+  menu_option: '4',
+  name: 'Clyde',
+  colour: 'Orange',
+  character: 'Pokey',
+  edible: 'false',
+}
+
+
+var ghosts = [inky, blinky, pinky, clyde]
+
+console.log(ghosts[0])
